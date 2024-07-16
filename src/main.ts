d44b512284e554ea04826feb40f571aa86681bde
Gashi Mentor <gme@sml.at>
2024-07-16 13:40:21 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+
+import App from './App.vue'
+import router from './router'
+
+const i18n = createI18n({
+    // something vue-i18n options here ...
+})
+
+const app = createApp(App)
+
+app.use(i18n)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
